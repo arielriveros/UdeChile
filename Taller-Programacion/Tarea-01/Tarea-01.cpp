c11c17bfdecd3a49da6970af547b9fa8422a687f
arielriveros <ariel.riveros@ing.uchile.cl>
2020-09-20 05:38:10 -0300

    Worms problem failed 1 test

--- Taller-Programacion/Tarea-01/Tarea-01.cpp
+++ Taller-Programacion/Tarea-01/Tarea-01.cpp
@@ -1,7 +1,5 @@
-// Tarea-01.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 /*
 input
@@ -25,22 +23,81 @@ ex
 		3
 */
 
-int main() {
+void wormsBS(int juicy, int floor, int roof, int* labels) {
+	while (floor <= roof) {
 
-	// inputs
+		int mid = (floor + roof) / 2;
+
+		if (juicy == labels[mid]) { cout << mid + 1 << endl; break; }
+
+		if (juicy < labels[mid]) {
+			if (juicy > labels[mid - 1]) {
+				cout << mid+1 << endl;
+				break;
+			}
+			if (juicy == labels[mid]) { cout << mid+1 << endl; break; }
+			else {
+				roof = mid-1;
+			}
+		}
+
+		else if (juicy > labels[mid]) {
+			if (juicy < labels[mid + 1]) {
+				cout << mid + 2 << endl;
+				break;
+			}
+			if (juicy == labels[mid]) { cout << mid+1 << endl; break; }
+			else {
+				floor = mid+1;
+			}
+		}
+	}
+}
+
+
+void wormsMain() {
+
+	// inits
+
 	int n = 0;
 	int m = 0;
+	int juicy = 0;
+	int maxLabel = 0;
+
+	// inputs
+
+	cin >> n;
 	int* piles = new int[n];
-	int* jworms = new int[m];
-	std::cin >> n;
-	for (int i = 0; i < n; i++) { std::cin >> piles[i]; }
-	std::cin >> m;
-	for (int j = 0; j < m; j++) { std::cin >> jworms[j]; }
-	std::cout << "n: " << n << "\n piles: " << 
-		*piles << "\n number of juicy worms: " << m 
-		<< "\n position of juicy worms: " << *jworms << "\n";
+	int* labels = new int[n];
 
-	// binary search
+	for (int i=0; i < n; ++i) { 
+		cin >> piles[i]; 
+		maxLabel += piles[i];
+		labels[i] = maxLabel;
+	}
 
+	cin >> m;
+	for (int j = 1; j <= m; j++) { 
+		cin >> juicy;
+		// binary search
+		wormsBS(juicy, 0, n, labels);
+	}
+}
+
+/*
+There are n benches in the Berland Central park. It is known that ai 
+people are currently sitting on the i-th bench. 
+Another m people are coming to the park and each of them is going to have a 
+seat on some bench out of n available.
+Let k be the maximum number of people sitting on one bench after 
+additional m people came to the park. Calculate the minimum possible 
+k and the maximum possible k.
+Nobody leaves the taken seat during the whole process.
+*/
+
+
+int main() {
+
+	wormsMain();
 	return 0;
 }
