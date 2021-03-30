d970bd98c2ed59b2483276e65da1c289aa7dac4e
arielriveros <ariel.riveros@ing.uchile.cl>
2021-03-30 08:10:42 -0300

    E1 Desarrollo aplicaciones web

---
+++ Diseno-Analisis-Algoritmos/CC4102/textSearch.cpp
@@ -0,0 +1,20 @@
+#include "CC4102.h"
+#include "textSearch.h"
+
+using namespace std;
+
+void textSearchController() {
+	string input;
+
+	string line_;
+	ifstream file_("./TestData/lorem-ipsum.txt");
+	
+	cout << "Text to search" << endl;
+	cin >> input;
+	cout << input << endl;
+}
+
+void bruteForce(std::string searchedText)
+{
+
+}
---
+++ Diseno-Analisis-Algoritmos/CC4102/textSearch.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void textSearchController();
+void bruteForce(std::string searchedText);
