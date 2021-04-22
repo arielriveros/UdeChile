3847f39b24319c2d2f56e2ff21b5263c8f1bc869
Ariel <ariel.riveros@ing.uchile.cl>
2021-04-22 16:27:55 -0400

    Apps-web Ejercicio2 Done

---
+++ Desarrollo-Aplicaciones-Web/2021-1/cgi-bin/save_doctor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# METADATA
+import cgi
+import cgitb
+cgitb.enable()
+utf8stdout = open(1, 'w', encoding='utf-8', closefd=False)
+print("Content-type:text/html\r\n\r\n")
+# END METADATA
+
+html_head = " <html><body><h3> Los datos fueron enviados correctamente </h3> "    
+html_end = "</body></html> "
+form = cgi.FieldStorage()
+print(html_head, file=utf8stdout)
+for key in form.keys():
+    print("<p>" + key + ": " + form[key].value + "</p>", file=utf8stdout)
+print(html_end, file=utf8stdout)
